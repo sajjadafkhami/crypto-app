7b03cc78c40898617330233c78250a6a28dcb9d3
sajad Afkhami <sajad.afkhami10@gmail.com>
2025-04-12 11:11:40 +0330

    add TableRow component & add style.

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import { ClipLoader } from "react-spinners";
+import TableRow from "./TableRow"
+import styles from "./TableCoin.module.css";
 
-function TableCoin({coins}) {
-    console.log(coins)
+
+function TableCoin({coins, isLoading}) {
+    //console.log(coins)
   return (
-    <div>TableCoin</div>
+    <div className={styles.container}>
+        {isLoading ? <ClipLoader color="#36d7b7" loading={true} size={50} /> : 
+        (<table className={styles.table}>
+            <thead>
+                <tr>
+                <th>Coin</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>24h</th>
+                <th>Total Value</th>
+                <th></th>
+                </tr>
+            </thead>
+            <tbody>{coins.map(coin =>
+            <TableRow coin={coin} key={coin.id}/>
+            )}
+            </tbody>
+        </table>)}
+    </div>
   )
 }
 
 export default TableCoin
+
